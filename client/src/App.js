2a9c17f33f9a677db31e20066fdeadc3da4b8ca1
dragonballf <73202702+dragonballf@users.noreply.github.com>
2020-10-25 21:14:33 +0900

    Add Get Customers API Module

--- client/src/App.js
+++ client/src/App.js
@@ -18,72 +18,60 @@ const styles = theme => ({
   table: {
     minWidth: 1080
   }
-})
-
-const customers = [
-  {
-    'id': '1',
-    'image': 'https://placeimg.com/64/64/1',
-    'name': '홍길동',
-    'birthday': '961222',
-    'gender': '남자',
-    'job': '대학생'
-  },
-  {
-    'id': '2',
-    'image': 'https://placeimg.com/64/64/2',
-    'name': '강주성',
-    'birthday': '981006',
-    'gender': '남자',
-    'job': '사원'
-  },
-  {
-    'id': '3',
-    'image': 'https://placeimg.com/64/64/3',
-    'name': '정광식',
-    'birthday': '861209',
-    'gender': '남자',
-    'job': '사장'
-  }
-]
+});
 
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+        .then(res => this.setState({customers: res}))
+        .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  };
+
   render() {
     const { classes } = this.props;
     return (
-      <div>
-        {
-          customers.map(c => {
-            return (
-              <Paper className={classes.root}>
-                <Table className={classes.table}>
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>번호</TableCell>
-                      <TableCell>사진</TableCell>
-                      <TableCell>이름</TableCell>
-                      <TableCell>생년월일</TableCell>
-                      <TableCell>성별</TableCell>
-                      <TableCell>직업</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    <Customer
-                      key={c.id}
-                      id={c.id}
-                      image={c.image}
-                      name={c.name}
-                      birthday={c.birthday}
-                      gender={c.gender}
-                      job={c.job}
-                    />
-                  </TableBody>
-                </Table>
-              </Paper>
-            )
-          })
-        }
-      </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>사진</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              this.state.customers ? this.state.customers.map(c => {
+                return (
+                  <Customer
+                    key={c.id}
+                    id={c.id}
+                    image={c.image}
+                    name={c.name}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                  />
+                );
+              }): ""
+            }
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({ message: 'Hello Express!' });
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': '1',
+            'image': 'https://placeimg.com/64/64/1',
+            'name': '홍길동',
+            'birthday': '961222',
+            'gender': '남자',
+            'job': '대학생'
+        },
+        {
+            'id': '2',
+            'image': 'https://placeimg.com/64/64/2',
+            'name': '강주성',
+            'birthday': '981006',
+            'gender': '남자',
+            'job': '사원'
+        },
+        {
+            'id': '3',
+            'image': 'https://placeimg.com/64/64/3',
+            'name': '정광식',
+            'birthday': '861209',
+            'gender': '남자',
+            'job': '사장'
+        }
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
